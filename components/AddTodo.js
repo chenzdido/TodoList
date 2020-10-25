6d71929232e6fa43d64050fea0315af1a7dfcb28
chenzdido <chenzhedido@outlook.com>
2020-10-25 12:07:35 +0800

    [zhe] feat:add actions & reducers.

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput, Button} from 'react-native';
 
 class AddTodo extends Component {
   render() {
     return (
       <>
         <View>
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          />
+          <Button title="Add" />
           <Text>AddTodo</Text>
         </View>
       </>
